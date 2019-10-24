24b671f7c92e1594261f4480be657dbb1192ef1e
bobpepers <bob.pepers@gmail.com>
2019-10-24 11:16:35 +0200

    up

--- client/src/app/components/ParticlesRunebase.jsx
+++ client/src/app/components/ParticlesRunebase.jsx
@@ -11,50 +11,34 @@ export default class ParticlesRunebase extends Component{
           skyColor: 0x70a0c,
           cloudColor: 0x2f4d7a,
           sunColor: 0xae7e42,
-          sunlightColor: 0x4f2e0e
+          sunlightColor: 0x4f2e0e,
+          scaleMobile: 1,
+          scale: 1,
+          speed: 1,
         })
-
-
     }
 
     render(){
         return (
             <section id='topsection' className="top">
             	<div className="position-relative">
-                    <div id="foglayer_01" class="fog zIndex-3">
-                      <div class="image01"></div>
-                      <div class="image02"></div>
+                    <div id="foglayer_01" className="fog zIndex-3">
+                      <div className="image01"></div>
+                      <div className="image02"></div>
                     </div>
-                    <div id="foglayer_02" class="fog zIndex-3">
-                      <div class="image01"></div>
-                      <div class="image02"></div>
+                    <div id="foglayer_02" className="fog zIndex-3">
+                      <div className="image01"></div>
+                      <div className="image02"></div>
                     </div>
-                    <div id="foglayer_03" class="fog zIndex-3">
-                      <div class="image01"></div>
-                      <div class="image02"></div>
+                    <div id="foglayer_03" className="fog zIndex-3">
+                      <div className="image01"></div>
+                      <div className="image02"></div>
                     </div>
                     <div id='clouds'>
                     </div>
             		<div id="particles-js">
                 		<img className="nopointer position-absolute bot-0" src={Mountain} alt="" />
-                		<Particles
-                          params={{
-                                particles: {
-                                    line_linked: {
-                                        shadow: {
-                                            enable: true,
-                                            color: "#3CA9D1",
-                                            blur: 5
-                                        }
-                                    }
-                                }
-                            }}
-                          style={{
-                            width: '100%',
-                            height: '100%',
-                            backgroundColor: 'rgb(15, 7, 8)',
-                          }}
-                        />
+
                 	</div>
 
                 	<div className="main_top_area shadow-w nopointer position-absolute row justify-content-center .zIndex-4">
